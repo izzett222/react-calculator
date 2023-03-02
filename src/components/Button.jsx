f64a6cac93668ae4c53aaca96b5bafe7036584fc
izzett222 <iizzeddin62@gmail.com>
2023-03-02 17:26:49 +0200

    modify large button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,3 @@
 export default function Button({children, bg, color, large, onClick}) {
-    return <button onClick={onClick} className={`flex items-center justify-center w-full aspect-square border border-gray-600 ${color} ${bg} ${large && 'col-span-2 aspect-[initial]'}`}>{children}</button>
+    return <button onClick={onClick} className={`flex items-center justify-center w-full  border border-gray-600 ${color} ${bg} ${large ? 'col-span-2 aspect-[initial]' : 'aspect-square'}`}>{children}</button>
 }
